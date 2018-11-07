dbc09a1429d89cab51ba610f1c0a0a02de8462f9
yczhang1017 <zhangyuchen12344@gmail.com>
2018-11-07 01:10:26 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -73,7 +73,7 @@ def test_net(save_folder, net, cuda, testset, transform, thresh):
                 coords = (pt[0], pt[1], pt[2], pt[3])
                 pred_num += 1
                 with open(filename, mode='a') as f:
-                    f.write(str(pred_num)+' label: '+label_name+' score: ' +
+                    f.write(str(pred_num)+' label: '+label_name+'('+str(i-1)+') score: ' +
                             str(score) + ' '+' || '.join(str(c) for c in coords) + '\n')
                 j += 1
 
