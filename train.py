b3bcaf3148d229be40c1df71ee50730c18f60442
yczhang1017 <zhangyuchen12344@gmail.com>
2018-11-12 13:19:19 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -119,8 +119,8 @@ def train():
         ssd_net.loc.apply(weights_init)
         ssd_net.conf.apply(weights_init)
 
-    #optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum,weight_decay=args.weight_decay)
-    optimizer = optim.Adam(net.parameters(),lr=args.lr,weight_decay=args.weight_decay)
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum,weight_decay=args.weight_decay)
+    #optimizer = optim.Adam(net.parameters(),lr=args.lr,weight_decay=args.weight_decay)
     criterion = MultiBoxLoss(cfg['num_classes'], 0.5, True, 0, True, 3, 0.5,
                              False, args.cuda)
 
